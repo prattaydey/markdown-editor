7599c1a40b04b8e99761737d5e8df6d5e49437a9
Prattay Dey <prattaydey@Prattays-MacBook-Pro.local>
2024-08-06 01:40:57 +0600

    finished prev frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Editor from "./components/Editor"
 import Preview from "./components/Preview"
+import Navbar from "./components/Navbar";
 
 function App() {
   const storedMarkdown = localStorage.getItem("MARKDOWN");
+  console.log(storedMarkdown)
+  const [markdown, setMarkdown] = useState(storedMarkdown ? storedMarkdown : '');
 
-  const [markdown, setMarkdown] = useState(storedMarkdown ? storedMarkdown : '# Markdown Editor');
+  useEffect(() => {
+    if (!storedMarkdown) {
+      fetch('/defaulttext.txt')
+        .then(response => response.text())
+        .then(text => setMarkdown(text));
+    }
+  }, [storedMarkdown]);
 
   const callback = (markdown: string) => {
     setMarkdown(markdown);
-    localStorage.setItem("MAKRDOWN", markdown);
+    localStorage.setItem("MARKDOWN", markdown);
   }
-  
   return (
-    <main className="w-full h-screen grid grid-cols-1 sm:grid-cols-2 bg-zinc-900 text-gray-300">
+    <main className="w-full bg-zinc-900 text-gray-300">
+      <Navbar />
+      <div className = "h-screen grid grid-cols-1 sm:grid-cols-2">
       <Editor markdown={markdown} setMarkdown={callback}/>
       <Preview markdown={markdown} />
+      </div>
     </main>
   )
 }
