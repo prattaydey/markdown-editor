750d4e31e97676325d38ef176a94d2487873d954
Prattay Dey <prattaydey@Prattays-MacBook-Pro.local>
2024-09-05 01:45:51 -0400

    upd again

--- frontend/src/apiConfig.ts
+++ frontend/src/apiConfig.ts
@@ -1,7 +1,3 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const API_BASE_URL: string = import.meta.env.REACT_APP_API_URL || "";
+const API_BASE_URL: string = import.meta.env.VITE_API_URL || "";
 
 export default API_BASE_URL;
